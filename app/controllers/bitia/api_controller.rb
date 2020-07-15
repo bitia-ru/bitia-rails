47cd98ee332c33aca22371a87891836ce9b37013
Artem Levenkov <alev@bitia.ru>
2020-07-15 23:21:32 +0300

    Fix PurableController#<resource_name> method

--- app/controllers/bitia/api_controller.rb
+++ app/controllers/bitia/api_controller.rb
@@ -1,164 +1,154 @@
 # frozen_string_literal: true
 
-class Bitia::ApiController < ActionController::Base
-  include ActionController::MimeResponds
+module Bitia
+  class ApiController < ActionController::Base
+    include ActionController::MimeResponds
 
-  respond_to :json
+    respond_to :json
 
-  include Resourceable
+    include Resourceable
 
-  before_action :purable_initialize_metavars
-  before_action :purable_prepare_resources
+    before_action :purable_initialize_metavars
+    before_action :purable_prepare_resources
 
-  def index
-    all = resources.count
-    if params.include? :offset
-      instance_variable_set("@#{resources_name}", resources.offset(params[:offset]))
+    def index
+      all = resources.count
+      if params.include? :offset
+        instance_variable_set("@#{resources_name}", resources.offset(params[:offset]))
+      end
+      if params.include? :limit
+        instance_variable_set("@#{resources_name}", resources.limit(Integer(params[:limit])))
+      end
+      @metadata[:all] = all unless @metadata[:all]
+
+      render
     end
-    if params.include? :limit
-      instance_variable_set("@#{resources_name}", resources.limit(Integer(params[:limit])))
+
+    def new
+      authorize resource
+      render
     end
-    @metadata[:all] = all unless @metadata[:all]
 
-    render
-  end
+    def show
+      authorize resource
+      render
+    end
 
-  def new
-    authorize resource
-    render
-  end
+    def create
+      pure_create_or_update
+      render
+    end
 
-  def show
-    authorize resource
-    render
-  end
+    def update
+      pure_create_or_update
+      render
+    end
 
-  def create
-    pure_create_or_update
-    render
-  end
+    def destroy
+      authorize resource
+      resource.destroy!
 
-  def update
-    pure_create_or_update
-    render
-  end
+      render
+    end
 
-  def destroy
-    authorize resource
-    resource.destroy!
+    def pure_filter(param)
+      return unless params[param].present?
 
-    render
-  end
+      instance_variable_get("@#{resources_name}").where!(param => params[param])
+    end
 
-  def pure_filter(param)
-    return unless params[param].present?
+    helper_method :pure_filter
 
-    instance_variable_get("@#{resources_name}").where!(param => params[param])
-  end
+    private
 
-  helper_method :pure_filter
+    def purable_initialize_metavars
+      @resource = purable_model.name.underscore.to_sym
+      @metadata ||= {}
+      nil
+    end
 
-  def purable_model
-    self.class.send(:purable_model)
-  end
+    def purable_prepare_resources
+      @current_user = User.find(params[:user_id]) if params.include? :user_id
 
-  helper_method :purable_model
+      if params[:action] == 'index'
+        instance_variable_set("@#{resources_name}", purable_relation.all)
+      elsif params[:action] == 'show'
+        id = params[:id].present? ? params[:id] : purable_param_id
 
-  def purable_model_chain
-    self.class.send(:purable_model_chain)
-  end
+        instance_variable_set(
+          "@#{resource_name}", id ? purable_relation.find(id) : purable_relation.new
+        )
+      elsif purable_resource_params.is_a?(Array)
+        resources = purable_resource_params.map do |params|
+          if params[:id].present? && params[:id] != 'self'
+            purable_relation.find(params[:id]).tap { |r| r.assign_attributes(params) }
+          else
+            purable_relation.new(params)
+          end
+        end
 
-  helper_method :purable_model
+        instance_variable_set("@#{resources_name}", resources)
+      else
+        resource = purable_relation.new(purable_resource_params)
+        instance_variable_set("@#{resource_name}", resource)
+      end
+    end
 
-  private
+    def purable_param_id
+      purable_resource_params[:id]
+    rescue StandardError
+      nil
+    end
 
-  def purable_initialize_metavars
-    @resource = purable_model.name.underscore.to_sym
-    @metadata ||= {}
-    nil
-  end
-
-  def purable_prepare_resources
-    @current_user = User.find(params[:user_id]) if params.include? :user_id
-
-    if params[:action] == 'index'
-      instance_variable_set("@#{resources_name}", purable_relation.all)
-    elsif params[:action] == 'show'
-      id = params[:id].present? ? params[:id] : purable_param_id
-
-      instance_variable_set(
-        "@#{resource_name}", id ? purable_relation.find(id) : purable_relation.new
-      )
-    elsif purable_resource_params.is_a?(Array)
-      resources = purable_resource_params.map do |params|
-        if params[:id].present? && params[:id] != 'self'
-          purable_relation.find(params[:id]).tap { |r| r.assign_attributes(params) }
-        else
-          purable_relation.new(params)
+    def purable_relation
+      purable_model_chain.inject(nil) do |m, pm|
+        if m.nil?
+          pm.singularize.classify.constantize
+        elsif params.include?("#{m.to_s.underscore}_id")
+          m.find(params["#{m.to_s.underscore}_id"]).send(pm.to_sym) # TODO: uncovered
         end
       end
-
-      instance_variable_set("@#{resources_name}", resources)
-    else
-      resource = purable_relation.new(purable_resource_params)
-      instance_variable_set("@#{resource_name}", resource)
     end
-  end
 
-  def purable_param_id
-    purable_resource_params[:id]
-  rescue StandardError
-    nil
-  end
-
-  def purable_relation
-    purable_model_chain.inject(nil) do |m, pm|
-      if m.nil?
-        pm.singularize.classify.constantize
-      elsif params.include?("#{m.to_s.underscore}_id")
-        m.find(params["#{m.to_s.underscore}_id"]).send(pm.to_sym) # TODO: uncovered
+    def pure_create_or_update
+      if resources.present?
+        purable_model.transaction { resources.each(&:save!) }
+      else
+        resource.save!
       end
     end
-  end
 
-  def pure_create_or_update
-    if resources.present?
-      purable_model.transaction { resources.each(&:save!) }
-    else
-      resource.save!
-    end
-  end
-
-  def purable_resource_params
-    send :"#{resource_name}_params"
-  end
-
-  def self.purable_model_chain
-    controller_path = self.controller_path.dup
-
-    if defined?(controller_prefix) and controller_prefix.present?
-      prefix_s = controller_prefix.map(&:to_s).join('/')
-      raise StandardError, 'Bad prefix' if controller_path.index(prefix_s) != 0
-
-      controller_path.sub! "#{prefix_s}/", ''
+    def purable_resource_params
+      send :"#{resource_name}_params"
     end
 
-    controller_path.split('/')
+    def purable_model
+      purable_model_chain.last.singularize.classify.constantize
+    end
+
+    def purable_model_chain
+      controller_path = self.class.controller_path.dup
+
+      if defined?(self.class.controller_prefix) and self.class.controller_prefix.present?
+        prefix_s = self.class.controller_prefix.map(&:to_s).join('/')
+        raise StandardError, 'Bad prefix' if controller_path.index(prefix_s) != 0
+
+        controller_path.sub! "#{prefix_s}/", ''
+      end
+
+      controller_path.split('/')
+    end
+
+    def initialize
+      super
+
+      resource_accessor_name = send(:purable_model).name.underscore.to_sym
+      define_singleton_method(resource_accessor_name) { resource }
+      # rubocop:disable Style/AccessModifierDeclarations
+      singleton_class.class_eval { private resource_accessor_name }
+      # rubocop:enable Style/AccessModifierDeclarations
+    end
+
+    private_class_method :inherited
   end
-
-  private_class_method :purable_model_chain
-
-  def self.purable_model
-    purable_model_chain.last.singularize.classify.constantize
-  end
-
-  private_class_method :purable_model
-
-  def self.inherited(klass)
-    resource_accessor_name = klass.send(:purable_model).name.underscore.to_sym
-    klass.define_method(resource_accessor_name) { resource }
-    klass.helper_method resource_accessor_name
-  end
-
-  private_class_method :inherited
 end
